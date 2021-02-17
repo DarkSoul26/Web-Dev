352217cd41b911f15a9a7ca5c1beebbb60134e55
DarkSoul26 <sanchitkalsi@gmail.com>
2021-02-17 12:38:54 +0530

    Add Env Vars

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //jshint esversion:6
 
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 const app = express();
 
 app.use(express.static("public"));
@@ -14,10 +16,12 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});   //one of the methods to encrypt (extra package)
 
 const User = new mongoose.model("User", userSchema);
 
